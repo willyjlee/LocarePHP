6f0a7daf59d33c26a1e614478407e3bcfe565467
Cai Yeo <joominyeo@outlook.com>
2016-10-01 15:42:39 -0700

    Updated databse password.

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "password"); // db password (mention your db password here)
-define('DB_DATABASE', "database"); // database name
+define('DB_PASSWORD', "passme"); // db password (mention your db password here)
+define('DB_DATABASE', "locare"); // database name
 define('DB_SERVER', "localhost"); // db server
 
 ?>
